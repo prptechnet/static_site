759008cd301ff3a70f0cacbe22f4ced6cf1349ae
Paul Paton <prptechnet.protonmail.com>
2025-05-16 08:36:48 +0100

    updated testnode and main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello world!")
+from textnode import *
+
+def main():
+   print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
+
+if __name__ == '__main__':
+    main()
